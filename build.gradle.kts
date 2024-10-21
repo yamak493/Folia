7b534ef0bd2de16e4bb93753ca59602dcf64c424
Spottedleaf <Spottedleaf@users.noreply.github.com>
2024-10-21 09:30:58 -0700

    Update Paper

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import io.papermc.paperweight.tasks.RebuildGitPatches
 plugins {
     java
     `maven-publish`
-    id("io.papermc.paperweight.patcher") version "1.7.1"
+    id("io.papermc.paperweight.patcher") version "1.7.3"
 }
 
 val paperMavenPublicUrl = "https://repo.papermc.io/repository/maven-public/"
